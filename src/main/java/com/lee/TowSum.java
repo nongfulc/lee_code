e820df2e9fcb567a485fe4670936964c208f070f
lc_666 <alu920307@gmail.com>
2020-10-27 21:40:06 +0800

    two sum

--- src/main/java/com/lee/TowSum.java
+++ src/main/java/com/lee/TowSum.java
@@ -2,11 +2,29 @@ package com.lee;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class TowSum {
 
 
     @Test
     public void alu() {
+        int[] nums = {3, 3, 6};
+        int[] result = twoSumL(nums, 6);
+        System.out.println(Arrays.toString(result));
+    }
 
+    public int[] twoSumL(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int another = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (another == nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
     }
 }
